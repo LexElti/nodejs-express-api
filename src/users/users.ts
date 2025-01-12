bcd7de0f462e613c39d1dd46247c7379aaa30707
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-01-12 17:27:52 +0300

    Класс приложения

--- src/users/users.ts
+++ src/users/users.ts
@@ -3,16 +3,16 @@ import express from 'express';
 const userRouter = express.Router();
 
 userRouter.use((req, res, next) => {
-	console.log('Обработчик users');
-	next();
+  console.log('Обработчик users');
+  next();
 });
 
 userRouter.post('/login', (req, res) => {
-	res.send('login');
+  res.send('login');
 });
 
 userRouter.post('/register', (req, res) => {
-	res.send('register');
+  res.send('register');
 });
 
 export { userRouter };
