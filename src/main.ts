a3e1643cbfa83fe89c6f5d8301905ae56164e345
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-01-20 00:03:47 +0300

    Внедрение ContainerModule

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-import { Container } from 'inversify';
+import { Container, ContainerModule, interfaces } from 'inversify';
 import { App } from './app';
 import { ExceptionFilter } from './errors/exception.filter';
 import { IExceptionFilter } from './errors/exception.filter.interface';
 import { LoggerService } from './logger/logger.service';
+import { IUsersController } from './users/users.controller.interface';
 import { UsersController } from './users/users.controller';
 import { ILogger } from './logger/logger.interface';
 import { TYPES } from './types';
 
-const appContainer = new Container();
-appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
-appContainer.bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
-appContainer.bind<UsersController>(TYPES.UsersController).to(UsersController);
-appContainer.bind<App>(TYPES.Application).to(App);
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+  bind<ILogger>(TYPES.ILogger).to(LoggerService);
+  bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+  bind<IUsersController>(TYPES.UsersController).to(UsersController);
+  bind<App>(TYPES.Application).to(App);
+});
 
-const app = appContainer.get<App>(TYPES.Application);
-app.init();
+function bootstrap() {
+  const appContainer = new Container();
+  appContainer.load(appBindings);
 
-export { app, appContainer };
+  const app = appContainer.get<App>(TYPES.Application);
+  app.init();
+
+  return { app, appContainer };
+}
+
+export const { app, appContainer } = bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,11 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { inject, injectable } from 'inversify';
 import 'reflect-metadata';
 import { BaseController } from '../common/base.controller';
+import { IUsersController } from './users.controller.interface';
 import { ILogger } from '../logger/logger.interface';
 import { TYPES } from '../types';
 
 @injectable()
-export class UsersController extends BaseController {
+export class UsersController extends BaseController implements IUsersController {
   constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
     super(loggerService);
     this.bindRoutes([
